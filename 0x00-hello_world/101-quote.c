8c9966c9abf6b833748f7d95f9290ff75250dea3
marwannour1 <marwannour20022016@gmail.com>
2023-07-15 12:27:36 -0700

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, str, strlen(str));
+	return (1);
+}
