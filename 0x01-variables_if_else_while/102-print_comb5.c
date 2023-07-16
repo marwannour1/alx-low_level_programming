e5eed32a6c44b81a78a2f9f64df80a217776b3d5
marwannour1 <marwannour20022016@gmail.com>
2023-07-15 19:14:16 -0700

    c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,25 +10,32 @@
 int main(void)
 {
 	int i = '0';
-	int j;
-	int k;
+	int j = '0';
+	int k = '0';
+	int l;
 
-	while (i < '8')
+
+	while (i <= '9')
 	{
-		j = i + 1;
 		while (j <= '8')
 		{
-			k = j + 1;
 			while (k <= '9')
 			{
-
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i != '7')
+				l = j + 1;
+				while (l <= '9')
 				{
-					putchar(',');
+
+					putchar(i);
+					putchar(j);
 					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					l += 1;
 				}
 				k += 1;
 			}
