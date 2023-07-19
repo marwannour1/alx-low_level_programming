ca9f14957d5d40df4cfc5ae42535957549a310cf
marwannour1 <marwannour20022016@gmail.com>
2023-07-19 11:41:05 -0700

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *print_alphabet - print a-z using _putchar 10 times
+  *
+  */
+void print_alphabet(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++){
+	
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
