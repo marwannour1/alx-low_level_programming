c320ce720fa2de5bafec2ca71cbc810c32e15261
marwannour1 <marwannour20022016@gmail.com>
2023-07-19 08:43:11 -0700

    fixed bug

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,9 @@
 int main(void)
 {
 	char str[] = "_putchar";
-	for (int i = 0; i < 8; i++)
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,5 +7,6 @@
   * Return: on success 1, on error -1
   */
 int _putchar(char c)
-
+{
 	return (write(1, &c, 1));
+}
