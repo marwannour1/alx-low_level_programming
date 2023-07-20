530a90ce895c17c968ddb3687aa3c04ccf7d05fc
marwannour1 <marwannour20022016@gmail.com>
2023-07-19 18:32:24 -0700

    c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * times_table - last digit
+  *
+  *
+  * Return: last digit of number
+  *
+  */
+void times_table(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			_putchar('0' + (num1 * num2));
+			_putchar(',');
+			if (num1 * num2 < 10)
+				_putchar(' ');
+			_putchar(' ');
+		}
+	}
+}
