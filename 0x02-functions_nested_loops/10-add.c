8a9cfc167f4e7d02ae1a71e5d1f8615aa7694e0f
marwannour1 <marwannour20022016@gmail.com>
2023-07-19 19:22:22 -0700

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - check if letter lowercase
+  *
+  * @num1: number 1
+  * @num2: number 2
+  *
+  *Return: 1 if c is lower and 0 if not lower
+  *
+  */
+int add(int num1, num2)
+{
+	return (num1 + num2);
+}
