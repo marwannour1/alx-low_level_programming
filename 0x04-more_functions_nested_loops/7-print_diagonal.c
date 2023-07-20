f73785e5afd44b2689ed47b379907dbeb15268f4
marwannour1 <marwannour20022016@gmail.com>
2023-07-20 11:14:07 -0700

    c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_diagonal - checks if letter is upper
+  * @n: number times
+  *
+  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('/');
+		putchar('\n');
+	}
+}
