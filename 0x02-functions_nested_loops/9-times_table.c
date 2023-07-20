b93d7ced94322d7e142260dfef3bb8b395c37ee2
marwannour1 <marwannour20022016@gmail.com>
2023-07-19 18:44:13 -0700

    c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 	{
 		for (num2 = 0; num2 < 10; num2++)
 		{
-			if (num1 * num2 > 10)
+			if (num1 * num2 >= 10)
 			{
 				_putchar('0' + (num1 * num2) / 10);
 				_putchar('0' + (num1 * num2) % 10);
