6297dcc5c2e6666e16d281bd6be7f0a856c1ca0a
marwannour1 <marwannour20022016@gmail.com>
2023-07-20 18:50:40 -0700

    c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,9 +10,11 @@ void print_triangle(int size);
 	int i, j, k;
 
 	for (i = 0; i < size; i++)
+	{
 		for (j = size - 1; j > 0; j--)
 			_putchar(' ');
 		for (k = 0; k <= i; k++)
 			_putchar('#');
 		_putchar('\n');
+	}
 }
