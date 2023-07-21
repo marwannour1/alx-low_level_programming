bd1e03abec77ab940ece5e78623fcc6e125f7e2e
marwannour1 <marwannour20022016@gmail.com>
2023-07-20 18:27:02 -0700

    c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - checks if letter is upper
+  * 
+  * Return: Always 0 (success)
+  *
+  */
+void main()
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i)
+	}
+	
+}
