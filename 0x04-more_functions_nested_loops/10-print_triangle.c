1290411da5538b89ccb1c59cfeb4d72e47d46e70
marwannour1 <marwannour20022016@gmail.com>
2023-07-20 19:00:40 -0700

    c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 
 /**
   * print_triangle - checks if letter is upper
-  * size: size
+  * @size: size
   *
   */
 void print_triangle(int size)
@@ -11,7 +11,7 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = size - 1; j > 0; j--)
+		for (j = size - 1; j > i; j--)
 			_putchar(' ');
 		for (k = 0; k <= i; k++)
 			_putchar('#');
