062c92fdfedb8b0abb5132e0c6a3e99eb2e57817
marwannour1 <marwannour20022016@gmail.com>
2023-07-20 18:44:04 -0700

    c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * main - checks if letter is upper
+  * size: size
+  *
+  * Return: Always 0 (success)
+  *
+  */
+void print_triangle(int size);
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+		for (j = size - 1; j > 0; j--)
+			_putchar(" ");
+		for (k = 0; k <= i; k++)
+			_putchar("#");
+		_putchar("\n")
+
+	return (0);
+}
