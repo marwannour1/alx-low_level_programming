39f38c6d5768091e2c7d0026f872e5da0a4f982a
marwannour1 <marwannour20022016@gmail.com>
2023-07-22 07:46:34 -0700

    c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,9 @@ void print_times_table(int num)
 	int num1;
 	int num2;
 
+	if (num > 15 || num < 0)
+		return;
+
 	for (num1 = 0; num1 <= num; num1++)
 	{
 		for (num2 = 0; num2 <= num; num2++)
@@ -32,7 +35,8 @@ void print_times_table(int num)
 			}
 			if (num1 * (num2 + 1) < 10)
 				_putchar(' ');
-			_putchar(' ');
+			if (num1 * (num2 + 1) < 100)
+				_putchar(' ');
 			_putchar(' ');
 		}
 		_putchar('\n');
