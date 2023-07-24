4529fc53489c58327c0f73d55bc22d2ed8c41a77
marwannour1 <marwannour20022016@gmail.com>
2023-07-24 04:36:29 -0700

    c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_number - Entry point
+  * @n: number to be processed
+  */
+void print_number(int n)
+{
+	int divisor = 1;
+	int num = n;
+	int digit;
+
+	if (num < 0)
+		_putchar('-');
+	while (num < -9 || num > 9)
+	{
+		num /= 10;
+		divisor *= 10;
+	}
+	while (divisor != 1)
+	{
+		digit = n / divisor;
+		n = n - digit * divisor;
+		_putchar('0' + digit);
+		divisor /= 10;
+	}
+}
