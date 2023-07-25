fbc093b9743acbee8600c93570b79a4348d6e7ec
marwannour1 <marwannour20022016@gmail.com>
2023-07-25 10:14:01 -0700

    c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _puts - c
+  * @str: p
+  *
+  * Return: 98
+  */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n')
+}
