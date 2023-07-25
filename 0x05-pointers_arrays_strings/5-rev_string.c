30d294576799985b3d43ac02e7bcc8421bc5bfec
marwannour1 <marwannour20022016@gmail.com>
2023-07-25 10:38:09 -0700

    c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_rev - c
+  * @s: p
+  *
+  * Return: 98
+  */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char tmp;
+
+	while (str[i])
+		i++;
+
+	while (j++ < i / 2)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+	}
+	_putchar('\n');
+}
