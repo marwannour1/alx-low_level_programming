76776352199ed37e326916cfdc90cfc808a3f84b
marwannour1 <marwannour20022016@gmail.com>
2023-07-25 13:49:01 -0700

    p

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _strcpy - c
+  * @a: p
+  * @n: n
+  * Return: 98
+  */
+char * _strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
