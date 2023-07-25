0a5c54b817fc92e48cdf31d704857d817137a720
marwannour1 <marwannour20022016@gmail.com>
2023-07-25 14:01:20 -0700

    c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,15 +2,22 @@
 
 /**
   * _strcpy - c
-  * @a: p
-  * @n: n
+  * @dest: p
+  * @src: n
   * Return: 98
   */
-char * _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int j;
 
 	for (i = 0; src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
+		j = i;
+	}
+	j++;
+	dest[j] = '\0';
+
 	return (dest);
 }
