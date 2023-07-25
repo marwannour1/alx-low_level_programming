c81c61e0d2eae3341f01f08691bd8fb80066d6ff
marwannour1 <marwannour20022016@gmail.com>
2023-07-25 11:34:29 -0700

    p

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * puts2 - c
+  * @str: p
+  *
+  * Return: 98
+  */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+}
