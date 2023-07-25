6ef39ff623777dc0b06acddbd5844adfbc968d96
marwannour1 <marwannour20022016@gmail.com>
2023-07-25 12:14:27 -0700

    c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * puts_half - c
+  * @str: p
+  *
+  * Return: 98
+  */
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+	char tmp;
+
+	while (s[length])
+		length++;
+	for (i = length / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
