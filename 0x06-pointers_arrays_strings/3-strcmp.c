11921ef93165c05e60e9a530b04b14bcab04c960
marwannour1 <marwannour20022016@gmail.com>
2023-07-27 09:43:54 -0700

    p

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strcmp - c
+  * @s1: p
+  * @s2: n
+  * Return: 98
+  */
+char *_strcmp(char *s1, char *s2)
+{
+	int i;
+	int cmp = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			cmp = (s1[i] - '0') - (s2[i] - '0');
+			break;
+		}
+	}
+
+	return (cmp);
+}
