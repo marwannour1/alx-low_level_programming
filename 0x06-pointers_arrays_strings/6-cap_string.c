7e1a9372ae3a78b19b61a976df9b58bbd617bf45
marwannour1 <marwannour20022016@gmail.com>
2023-07-27 16:37:48 -0700

    c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 	int i;
 	int j;
 	char delim[] = " \t\n,.!?\"(){}";
-	
+
 	if (str[0] >= 'a' && str[0] <= 'z')
 		str[i] = str[i] - 'a' + 'A';
 	for (i = 1; str[i]; i++)
