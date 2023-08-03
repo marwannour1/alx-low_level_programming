6472b834fa1c99afa890a321138567f21be60d87
marwannour1 <marwannour20022016@gmail.com>
2023-08-03 16:49:05 -0700

    R

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - c
+  * @s: p
+  * Return: 98
+  */
+void *_puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s++)
+}
