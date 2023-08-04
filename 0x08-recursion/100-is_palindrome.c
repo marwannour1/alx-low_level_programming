86c9ff1980a2309412c5c67437481f4843be67ea
marwannour1 <marwannour20022016@gmail.com>
2023-08-03 19:22:51 -0700

    R

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+int check(char *s, int start, int end);
+int index(char *s);
+
+/**
+  * is_palindrome - c
+  * @s: p
+  * Return: 98
+  */
+int is_palindrome(char *s)
+{
+	int end = index(s);
+
+	return (check(s, 0, end));
+}
+
+/**
+  * div - c
+  * @s: s
+  * @start: p
+  * @end: y
+  * Return: 98
+  */
+int check(int *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (check(s, start + 1, end - 1));
+}
+
+/**
+  * index - c
+  * @s: p
+  * Return: 98
+  */
+int index(int *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return (i);
+	return (index(s + 1) + 1);
