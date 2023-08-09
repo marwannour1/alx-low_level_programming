56e96a5c9e7a8d14e10c67880b6504aaa2087e5a
marwannour1 <marwannour20022016@gmail.com>
2023-08-09 16:31:53 -0700

    p

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	for (i = 0; i <= size1 + size2; i++)
 	{
-		if (i <= size1)
+		if (i < size1)
 			sp[i] = s1[i];
 		else
 			sp[i] = s2[i - size1];
