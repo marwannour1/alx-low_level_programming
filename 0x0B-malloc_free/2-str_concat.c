cf92303912696bb65d820072cf26d798e3ef56b8
marwannour1 <marwannour20022016@gmail.com>
2023-08-09 16:18:40 -0700

    p

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+
+/**
+  * _strdup - creates an arrar with malloc
+  * @s1: size of array
+  * @s2: s
+  *
+  * Return: a char
+  */
+char *_strdup(char *s1, char *s2)
+{
+	int size1 = 0;
+	int size2 = 0;
+	int i;
+	char *sp;
+
+	while (s1[size1])
+		size1++;
+	while (s2[size2])
+		size2++;
+	sp = malloc(size1 + size2 * sizeof(*s1) + 1);
+	for (int i = 0; i <= size1 + size2; i++)
+	{
+		if (i <= size1)
+			sp[i] = s1[i];
+		else
+			sp[i] = s2[i];
+	}
+	sp[i] = '\0';
+}
