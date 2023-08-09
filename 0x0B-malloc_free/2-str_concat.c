0c73aa3875c18d439c130b2856f3646b9e1762bd
marwannour1 <marwannour20022016@gmail.com>
2023-08-09 16:34:08 -0700

    p

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *sp;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "\0";
 	while (s1[size1])
 		size1++;
 	while (s2[size2])
