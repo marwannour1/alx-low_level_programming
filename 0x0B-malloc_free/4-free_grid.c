6ca0c47c26e08e910456b63782c28656c90f72f0
marwannour1 <marwannour20022016@gmail.com>
2023-08-09 17:21:43 -0700

    p

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+  * free_grid - creates an arrar with malloc
+  * @grid: size of array
+  * @height: s
+  *
+  * Return: a char
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(gird[i]);
+	free(grid);
+}
