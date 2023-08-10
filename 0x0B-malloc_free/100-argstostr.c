5dc915f4cc35430abbb209622008dc934e8d991a
marwannour1 <marwannour20022016@gmail.com>
2023-08-09 17:47:22 -0700

    p

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,16 +1,44 @@
 #include "main.h"
+/**
+  * _strlen - dhd
+  * @s: string
+  * Return: dhf
+  */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (s[size])
+		size++;
+	return (size);
+}
 
 /**
-  * free_grid - creates an arrar with malloc
-  * @grid: size of array
-  * @height: s
+  * *argstostr - creates an arrar with malloc
+  * @ac: size of array
+  * @av: s
   *
   * Return: a char
   */
-void free_grid(int **grid, int height)
+char *argstostr(int ac, char **av)
 {
-	int i;
-	for (i = 0; i < height; i++)
-		free(gird[i]);
-	free(grid);
+	char *sp;
+	int i, j, index
+	int totalsize = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++, totalsize++)
+		totalsize += _strlen(av[i]);
+	sp = malloc(totalsize * sizeof(*sp) + 1);
+	if (sp == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++, index++)
+			sp[index] = av[i][j];
+		sp[index] = '\n';
+		index++;
+	}
+	return (sp);
 }
