b048b8ba7ba6e326bcf60e5204758b870af0554a
marwannour1 <marwannour20022016@gmail.com>
2023-08-11 11:54:41 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * malloc_checked - creates an arrar with malloc
+  * @b: size of array
+  *
+  * Return: a char
+  */
+char *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == NULL)
+		exit(98);
+	return (m);
+}
