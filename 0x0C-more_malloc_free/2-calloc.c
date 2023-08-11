0718431c74d66e389169f1964a7ccaa6ec99f91f
marwannour1 <marwannour20022016@gmail.com>
2023-08-11 14:19:31 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,14 +9,13 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	unsigned int n = nmemb * size;
 	void *m = malloc(n);
 	char *pt = m;
 
 	if (m == NULL || size == 0 || nmemb == 0)
 		return (NULL);
-		while (n--)
-			*pt++ = (char) 0;
+	while (n--)
+		*pt++ = (char) 0;
 	return (m);
 }
