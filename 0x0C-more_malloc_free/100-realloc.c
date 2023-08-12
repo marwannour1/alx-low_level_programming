b8d8a53fb668673ca6d6f45aa75707bd50730e01
marwannour1 <marwannour20022016@gmail.com>
2023-08-11 18:06:27 -0700

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
-		return (p)
+		return (p);
 	}
 	else if(new_size > old_size)
 	{
