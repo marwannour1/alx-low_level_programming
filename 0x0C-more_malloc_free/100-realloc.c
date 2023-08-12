dd8f4326ea2bc37f337ac6e0ee7b88f75f56bbc6
marwannour1 <marwannour20022016@gmail.com>
2023-08-11 18:05:51 -0700

    malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+  * _realloc - creates an arrar with malloc
+  * @ptr: size of array
+  * @old_size: sid
+  * @new_size: dfd
+  *
+  * Return: a char
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	unsigned int i;
+	
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p)
+	}
+	else if(new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < old_size; i++)
+			*((char *)p + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (p);
+}
