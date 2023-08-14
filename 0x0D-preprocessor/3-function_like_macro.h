37d17aa90e7323c708c5f723353dd0ab329394c6
marwannour1 <marwannour20022016@gmail.com>
2023-08-14 07:33:35 -0700

    p

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,5 @@
-#ifndef ABS
-#define ABS(x) {((x) < 0 ? ((x) * -1) : (x))}
-#endif /* ABS */
+#ifndef MY_FUNC
+#define MY_FUNC
+#define ABS(x) \
+{((x) < 0 ? ((x) * -1) : (x))}
+#endif /* MY_FUNC */
