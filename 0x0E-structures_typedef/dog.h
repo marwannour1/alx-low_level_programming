ea95fd88ffef143eec407884024bdd9339728035
marwannour1 <marwannour20022016@gmail.com>
2023-08-14 16:29:22 -0700

    struc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG
 #define DOG
+/**
+  * struct dog - dhdhf
+  * @name: dhfh
+  * @age: hdhf
+  * @owner: dhfdh
+  *
+  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
 #endif /* DOG */
