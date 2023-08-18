50cc42a52f90a07cf561b518f661cb3aef36da4c
marwannour1 <marwannour20022016@gmail.com>
2023-08-18 14:41:04 -0700

    create functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+
+#include "variadic_functions.h"
+
+/**
+  * print_strings - sum args
+  * @separator: dhdhf
+  * @n: dfdf
+  * @...: dgd
+  *
+  * Return: djdj
+  */
+void print_strings(const char *separator, const unsigned int n,  ...)
+{
+	va_list ap;
+	int i = n;
+	char *str;
+
+	if (!n)
+	{
+		printf("\n")
+		return (0);
+	}
+	va_start(ap, n);
+	while (i--)
+	{
+		str = va_arg(ap, char *);
+		printf("%s%s", str ? str : "(nil)", i ? (separator ? separator : "") : "\n");
+	}
+	va_end(ap);
+
+}
