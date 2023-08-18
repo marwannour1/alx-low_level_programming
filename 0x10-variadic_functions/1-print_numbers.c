abcc822ab8a26318bacb4489af0695a310342fdd
marwannour1 <marwannour20022016@gmail.com>
2023-08-17 17:44:13 -0700

    v

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,20 +8,18 @@
   *
   * Return: djdj
   */
-int print_numbers(const char *separator, const unsigned int n, ...)
+void  print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	int i = n, sum = 0;
+	int i = n;
 
 	if (!n)
 	{
 		printf("\n");
-		return (0);
+		return;
 	}
 	va_start(ap, n);
 	while (i--)
 		printf("%d%s", va_arg(ap, int), i ? (separator ? separator : "") : "\n");
 	va_end(ap);
-	return (sum);
-
 }
