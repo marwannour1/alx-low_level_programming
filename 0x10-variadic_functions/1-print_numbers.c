53356244c12cd5e4bf2283fe8faa8f45ab39cb9c
marwannour1 <marwannour20022016@gmail.com>
2023-08-17 17:39:59 -0700

    v

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - sum args
+  * @n: dfdf
+  * @separator: dhfd
+  * @...: dgd
+  *
+  * Return: djdj
+  */
+int print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i = n, sum = 0;
+
+	if (!n)
+	{
+		printf("\n")
+		return (0);
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%d%s", va_arg(ap, int), i ? (separator ? separator : "") : "\n");
+	va_end(ap);
+	return (sum);
+
+}
