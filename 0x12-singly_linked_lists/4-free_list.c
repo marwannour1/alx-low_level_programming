496c4165a410eb1577d868f2cd774b7673ef92aa
marwannour1 <marwannour20022016@gmail.com>
2023-08-24 16:44:07 -0700

    single linked

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * free_list - print elements of a list_t
+  * @h: head of list
+  *
+  * Return: number of nodes
+  */
+void free_list(const list_t *h)
+{
+	list_t *node, *next_node;
+	if (!head)
+		return;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+}
+
