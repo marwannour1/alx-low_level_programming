cdb5822e40ecc040c46d788cdc93cfcd95249f42
marwannour1 <marwannour20022016@gmail.com>
2023-08-24 16:31:14 -0700

    single linked

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+
+#include "lists.h"
+
+/**
+  * add_node_end - print elements of a list_t
+  * @head: head of list
+  * @str: dghfd
+  * Return: number of nodes
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *new = malloc(sizeof(list_t));
+	list_t *temp = *head;
+
+	if (!head || !new)
+		return (NULL);
+	if (str)
+	{
+		new->str = strdup(str);
+		if (!new->str)
+		{
+			free(new);
+			return (NULL);
+		}
+		while (new->str[i])
+			i++;
+		new->len = i;
+	}
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	return (new);
+}
+
