fd7e0b41b1e6f920b75d20b14b61ddc00942d860
marwannour1 <marwannour20022016@gmail.com>
2023-08-24 16:11:55 -0700

    single linked

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,7 +6,7 @@
   * @str: dghfd
   * Return: number of nodes
   */
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
