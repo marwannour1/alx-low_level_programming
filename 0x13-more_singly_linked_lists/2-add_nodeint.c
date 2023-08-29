87a04876899e7e33e77b7e5b7f64bdf5916d606b
marwannour1 <marwannour20022016@gmail.com>
2023-08-29 15:44:31 -0700

    more

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,7 @@
   * @n: dghfd
   * Return: number of nodes
   */
-list_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 
