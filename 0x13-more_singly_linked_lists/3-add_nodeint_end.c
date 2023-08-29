0583a52af2a34dbaa360600a13dea019ce8ce081
marwannour1 <marwannour20022016@gmail.com>
2023-08-29 15:51:10 -0700

    more

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+  * add_nodeint_end - print elements of a list_t
+  * @head: head of list
+  * @n: dghfd
+  * Return: number of nodes
+  */
+listint_t *add_node_end(list_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+
+	if (!head || !new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (temp)
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+	else
+		*head = new;
+	return (new);
+}
+
