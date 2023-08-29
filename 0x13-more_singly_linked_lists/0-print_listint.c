babea501a7d7878fff2b577f4aaf89fcdb03aec0
marwannour1 <marwannour20022016@gmail.com>
2023-08-29 15:18:14 -0700

    more

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * print_list - print elements of a list_t
+  * print_listint - print elements of a list_t
   * @h: head of list
   *
   * Return: number of nodes
@@ -12,7 +12,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u\n", h->n);
+		printf("[%d\n", h->n);
 		h = h->next;
 		num_nodes++;
 	}
