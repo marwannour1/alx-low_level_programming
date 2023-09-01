b29faf5b6e6159814551a19392ac9e94d83e004c
marwannour1 <marwannour20022016@gmail.com>
2023-09-01 14:43:29 -0700

    bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * get_int - dhdhf
+  * @n: dhfh
+  * @index: dhfhd
+  * Return: dhfhd
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1 << index;
+
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	if (n & mask)
+		return (1);
+	return (0);
+}
