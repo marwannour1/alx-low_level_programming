0c1cc84aaf0216683770db9a1fe5512b294f7748
marwannour1 <marwannour20022016@gmail.com>
2023-09-01 15:24:17 -0700

    bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * clear_bit - dhdhf
+  * @n: dhfh
+  * @index: dhfhd
+  * Return: dhfhd
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = ~ (1 << index);
+
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	*n = (*n) & mask;
+	return (1);
+}
