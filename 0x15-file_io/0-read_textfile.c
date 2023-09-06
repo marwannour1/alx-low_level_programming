c868bd3500754679610eb9db72d5d646e5c5e97c
marwannour1 <marwannour20022016@gmail.com>
2023-09-06 03:59:52 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fD == -1)
 		return (0);
 	bytes = read(fD, buffer, letters);
-	write(STDOUT_FILENO, buffer, letters);
+	write(STDOUT_FILENO, buffer, bytes);
 	close(fD);
 	return (bytes);
 }
