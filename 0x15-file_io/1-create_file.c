aa506aa73bd462c7d4f0e8d31cdf882f78542c6d
marwannour1 <marwannour20022016@gmail.com>
2023-09-06 04:24:36 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,17 +27,16 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	char *buffer[8 * 1024];
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(*filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
 		return (1);
-	write(fd, *text_content, _strlen(text_content));
+	write(fd, text_content, _strlen(text_content));
 	close (fd);
 	return (1);
 }
