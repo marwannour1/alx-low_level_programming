bbc45d283b23f4ac3f0495bc691abfc8d197ceef
marwannour1 <marwannour20022016@gmail.com>
2023-09-06 14:56:08 -0700

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
   * @av: hdfhd
   * Return: dhfhd
   */
-void main(int ac, char **av)
+int main(int ac, char **av)
 {
 	int file_from, file_to;
 	ssize_t size;
@@ -14,26 +14,26 @@ void main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprint(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	file_to = open(av[1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	while ((size = read(file_from, buffer, 1024)) > 0)
 	{
-		if (size = write(file_to, buffer, 1024))
+		if ((size = write(file_to, buffer, 1024)))
 		{
-			dprint(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
@@ -41,12 +41,12 @@ void main(int ac, char **av)
 	file_to = close(file_to);
 	if (file_from)
 	{
-		dprint(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
 		exit(100);
 	}
 	if (file_to)
 	{
-		dprint(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
 		exit(100);
 	}
 	return (0);
