dd8bee33aea655877ce841ddabe53d4e101fbb6f
marwannour1 <marwannour20022016@gmail.com>
2023-09-06 03:50:39 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,11 +9,14 @@
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fD = open(filename, O_RDONLY);
+	int fD;
 	ssize_t bytes;
 	char buffer[1024 * 8];
 
-	if (filename == NULL || letters == 0 || fD == -1)
+	if (filename == NULL || letters == 0)
+		return (0);
+	fD = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 	bytes = read(fD, buffer, letters);
 	close(fD);
