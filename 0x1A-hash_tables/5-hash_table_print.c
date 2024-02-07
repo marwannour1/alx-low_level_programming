c4f6d8a759a9260138013ded67a5cd7cb83b41fb
marwannour1 <marwannour20022016@gmail.com>
2024-02-07 17:25:33 +0200

    1

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,32 +8,27 @@ void hash_table_print(const hash_table_t *ht)
 {
     hash_node_t *temp = NULL;
     unsigned long int i;
+    char flag = 0;
 
     if (ht == NULL || ht->array == NULL || ht->size == 0)
     {
         return;
     }
-
     printf("{");
     for (i = 0; i < ht->size; i++)
     {
         temp = ht->array[i];
         while (temp != NULL)
         {
-            printf("'%s': '%s'", temp->key, temp->value);
-            if (temp->next != NULL)
+            if (flag == 1)
             {
                 printf(", ");
             }
+
+            printf("'%s': '%s'", temp->key, temp->value);
+            flag = 1;
             temp = temp->next;
         }
-
-        if (i < ht->size - 1)
-        {
-            if (ht->array[i + 1] != NULL)
-            {
-                printf(", ");
-            }
-        }
     }
+    printf("}\n");
 }
