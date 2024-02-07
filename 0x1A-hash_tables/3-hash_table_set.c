39c1b858a64488cbb47de18f5cb8820f0ddfb861
marwannour1 <marwannour20022016@gmail.com>
2024-02-07 16:06:43 +0200

    1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,38 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - creates a new node
+ * @key: key
+ * @value: value
+ * Return: new node
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
  * @key: key
@@ -14,7 +46,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht == NULL || key == NULL || value == NULL || ht->array == NULL || ht->size == 0 || strlen(key) == 0)
+	if (ht == NULL || key == NULL || value == NULL || ht->array == NULL ||
+		ht->size == 0 || strlen(key) == 0)
 	{
 		return (0);
 	}
@@ -35,26 +68,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
 	{
-		free(new_node);
 		return (0);
 	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
-	new_node->next = NULL;
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
