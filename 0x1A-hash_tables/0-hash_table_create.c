68f557113d082ff0ec8ec5ea6dbfdae6a28a9676
marwannour1 <marwannour20022016@gmail.com>
2024-02-07 14:35:05 +0200

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *new_table = NULL;
+
+    if (size < 1)
+        return (NULL);
+
+    new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+    if (new_table == NULL)
+        return (NULL);
+
+    new_table->size = size;
+    new_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+    if (new_table->array == NULL)
+        return (NULL);
+
+    return (new_table);
+}
