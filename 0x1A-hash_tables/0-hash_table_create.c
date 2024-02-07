df43478837bd2ebc08bccd9e9d0d49e9f96be809
marwannour1 <marwannour20022016@gmail.com>
2024-02-07 14:42:12 +0200

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
+	hash_table_t *new_table = NULL;
 
-    if (size < 1)
-        return (NULL);
+	if (size < 1)
+		return (NULL);
 
-    new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+	new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-    new_table->size = size;
-    new_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-        return (NULL);
+	new_table->size = size;
+	new_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+		return (NULL);
 
-    return (new_table);
+	return (new_table);
 }
