abf057bb093062b2b60208ef78bdee052fcd676a
marwannour1 <marwannour20022016@gmail.com>
2024-02-07 15:29:54 +0200

    1

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,11 +10,11 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash;
+	unsigned long int hash;
 
-    if (key == NULL || size == 0)
-        return (0);
+	if (key == NULL || size == 0)
+		return (0);
 
-    hash = hash_djb2(key);
-    return (hash % size);
+	hash = hash_djb2(key);
+	return (hash % size);
 }
