7b0494082c3e235f694dad2b51003b6390aa553a
marwannour1 <marwannour20022016@gmail.com>
2024-02-07 16:18:51 +0200

    1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,14 +45,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *temp = NULL;
 	char *new_value;
 
-	index = key_index((const unsigned char *)key, ht->size);
-
 	if (ht == NULL || key == NULL || value == NULL || ht->array == NULL ||
 		ht->size == 0 || strlen(key) == 0)
 	{
+
 		return (0);
 	}
 
+	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
